ed3194560f0de5005482fd652a0d6416d77617b1
cjmin1233 <cjmin1233@gmail.com>
2022-11-15 13:25:21 +0900

    어빌리티 스크롤뷰 완료

--- My project/Assets/Prefabs/ScrollViewController.cs
+++ My project/Assets/Prefabs/ScrollViewController.cs
@@ -6,19 +6,15 @@ using UnityEngine.UI;
 public class ScrollViewController : MonoBehaviour
 {
     private ScrollRect scrollRect;
-    public float space = 50f;
+    float space = 50f;
     public GameObject uiPrefab;
-    public List<RectTransform> uiObjects = new List<RectTransform>();
-    // Start is called before the first frame update
-    void Start()
+    //public List<RectTransform> uiObjects = new List<RectTransform>();
+    public List<GameObject> AbilityList = new List<GameObject>();
+    [HideInInspector] public List<GameObject> SelectedAbilityList = new List<GameObject>();
+    /*
+    void Awake()
     {
         scrollRect = GetComponent<ScrollRect>();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
     public void AddNewUiObject()
     {
@@ -26,12 +22,55 @@ public class ScrollViewController : MonoBehaviour
         uiObjects.Add(newUi);
 
         float y = 0f;
-        for(int i = 0; i < uiObjects.Count; i++)
+        for (int i = 0; i < uiObjects.Count; i++)
         {
             uiObjects[i].anchoredPosition = new Vector2(0f, -y);
             y += uiObjects[i].sizeDelta.y + space;
         }
 
         scrollRect.content.sizeDelta = new Vector2(scrollRect.content.sizeDelta.x, y);
+    }
+     */
+    public void AddTest(GameObject gameObject)
+    {
+        if (scrollRect == null) scrollRect = GetComponent<ScrollRect>();
+        int index = gameObject.GetComponent<Ability>().index;
+        Debug.Log("The selected ability index is : " + index);
+        var newUi = Instantiate(AbilityList[index], scrollRect.content);
+        SelectedAbilityList.Add(newUi);
+
+        UpdateBook();
+        /*
+        GameObject newUi = Instantiate(gameObject, scrollRect.content);
+        newUi.SetActive(true);
+        AbilityList.Add(newUi);
+
+        float y = 0f;
+        for (int i = 0; i < AbilityList.Count; i++)
+        {
+            //uiObjects[i].anchoredPosition = new Vector2(0f, -y);
+            //y += uiObjects[i].sizeDelta.y + space;
+            RectTransform rectTransform = AbilityList[i].GetComponent<RectTransform>();
+            rectTransform.anchoredPosition = new Vector2(0f, -y);
+            y += rectTransform.sizeDelta.y + space;
+        }
+
+        scrollRect.content.sizeDelta = new Vector2(scrollRect.content.sizeDelta.x, y);
+        */
+    }
+    private void UpdateBook()
+    {
+        float y = 0f;
+        for (int i = 0; i < SelectedAbilityList.Count; i++)
+        {
+            //uiObjects[i].anchoredPosition = new Vector2(0f, -y);
+            //y += uiObjects[i].sizeDelta.y + space;
+            RectTransform rectTransform = SelectedAbilityList[i].GetComponent<RectTransform>();
+            rectTransform.anchoredPosition = new Vector2(0f, -y);
+            y += rectTransform.sizeDelta.y + space;
+        }
+
+        scrollRect.content.sizeDelta = new Vector2(scrollRect.content.sizeDelta.x, y);
+
     }
 }
